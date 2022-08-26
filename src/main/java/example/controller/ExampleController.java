22320aac3605e2364a94d19e98d10a52fd32218c
cmunknown <cmunknown@mail.ru>
2022-08-26 11:34:56 +0300

    v45642f

--- src/main/java/example/controller/ExampleController.java
+++ src/main/java/example/controller/ExampleController.java
@@ -15,6 +15,6 @@ public class ExampleController {
 
     @GetMapping
     public String getServiceName() {
-        return exampleService.getSelfName() + " hello MAXIM";
+        return exampleService.getSelfName() + " hello MAXIMX";
     }
 }
